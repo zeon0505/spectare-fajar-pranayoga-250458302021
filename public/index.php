f405e6f5832d987581ced6cfce68ee17a74d9fd7
zeon0505 <yogaodiy3334@gmail.com>
2025-12-09 11:08:35 +0700

    change index php

--- public/index.php
+++ public/index.php
@@ -16,7 +16,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-if (file_exists($maintenance = __DIR__.'/../laravel_core/storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
@@ -27,11 +27,11 @@ if (file_exists($maintenance = __DIR__.'/../laravel_core/storage/framework/maint
 |
 | Composer provides a convenient, automatically generated class loader for
 | this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
+| into the script here so we don\'t need to manually load our classes.
 |
 */
 
-require __DIR__.'/../laravel_core/vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -39,12 +39,12 @@ require __DIR__.'/../laravel_core/vendor/autoload.php';
 |--------------------------------------------------------------------------
 |
 | Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
+| the application\'s HTTP kernel. Then, we will send the response back
+| to this client\'s browser, allowing them to enjoy our application.
 |
 */
 
-$app = require_once __DIR__.'/../laravel_core/bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
